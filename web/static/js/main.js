d2012ea7594dd2aa27b170b0a415bfd012497cda
hmajoros <majoros.henry@gmail.com>
2014-08-05 15:47:02 -0400

    homepage naimations

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,35 +1,24 @@
-// (function() {
-
-//     var App = function() {
-//         this.
-//     };
-
-//     App.prototype.onLoaded = function() {
-//         // body...
-//     };
-
-// })();
-
-
-
 $(document).ready(function(){
     var nameString = "HENRY MAJOROS",
         nameArray = nameString.split("");
 
     for (var i = 0; i < nameArray.length; i++) {
-
-        $('#name').append('<div class="letter">' + nameArray[i] + '</div>');
+        $('#name').append('<div class="flip-container"><div class="tile"><div class="tile-front"></div><div class="tile-back">' + nameArray[i] + '</div></div></div>');
     }
+
+    $tiles = $('.flip-container');
+    setTimeout( function() {
+        $tiles.each(function(index) {
+            var el = $(this);
+            setTimeout( function(){ el.addClass('flipped'); }, index * 150);
+        });
+    }, 350);
+
 
     $('.header-topic').on('mouseover', function() {
         var leftPercent = this.getAttribute('data-attribute') * 20;
         $('#underline-hover').css('left', leftPercent + '%').show();
     });
-
-    // $('#header').on('mouseenter', function() {
-    //     var leftPercent = this.getAttribute('data-attribute') * 20;
-    //     $('#underline-hover').css('left', leftPercent + '%').show();
-    // });
 
     $('#header').on('mouseleave', function() {
         $('#underline-hover').hide();
@@ -39,6 +28,9 @@ $(document).ready(function(){
         var leftPercent = this.getAttribute('data-attribute') * 20;
 
         $('#underline').animate({left: leftPercent + '%'}, 200);
-    })
+    });
+
+
+
 });
 
