3e3a50996b40436a2e65770b860944b538f00e72
hmajoros <majoros.henry@gmail.com>
2014-09-04 19:23:41 -0400

    fonts and more anim

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,36 +1,74 @@
-$(document).ready(function(){
-    var nameString = "HENRY MAJOROS",
-        nameArray = nameString.split("");
+(function() {
 
-    for (var i = 0; i < nameArray.length; i++) {
-        $('#name').append('<div class="flip-container"><div class="tile"><div class="tile-front"></div><div class="tile-back">' + nameArray[i] + '</div></div></div>');
+    $(document).ready(onPageLoad());
+
+
+    function onPageLoad() {
+        /* bind event listeners */
+        $('.header-topic').on('mouseover', renderHeaderMouseover);
+        $('.header-topic').on('click', renderHeaderClick);
+        $('#header').on('mouseleave', renderHeaderMouseleave);
+        $(window).on('scroll', fadeCover);
+
+        /* call page load functions */
+        setCoverHeight();
+        printNameTiles();
+        flipNameTiles();
     }
 
-    $tiles = $('.flip-container');
-    setTimeout( function() {
-        $tiles.each(function(index) {
-            var el = $(this);
-            setTimeout( function(){ el.addClass('flipped'); }, index * 150);
+    function setCoverHeight() {
+        var screenHeight = $(window).height();
+        $('#cover').css({height: screenHeight});
+    }
+
+    function printNameTiles() {
+        var nameString = "HENRY MAJOROS",
+            nameArray = nameString.split("");
+
+        for (var i = 0; i < nameArray.length; i++) {
+            $('#name').append('<div class="flip-container"><div class="tile"><div class="tile-front"></div><div class="tile-back">' + nameArray[i] + '</div></div></div>');
+        }
+    }
+
+    function flipNameTiles() {
+        $('<img/>').attr('src', '/static/images/cover-full.jpg').load(function() {
+            $(this).remove(); // to prevent a memory leak
+            $('#cover').css('background-image', 'url(/static/images/cover-full.jpg)');
+
+            $tiles = $('.flip-container');
+            setTimeout( function() {
+                $tiles.each(function(index) {
+                    var el = $(this);
+                    setTimeout( function(){ el.addClass('flipped'); }, index * 150);
+                });
+            }, 200);
         });
-    }, 350);
+    }
 
-
-    $('.header-topic').on('mouseover', function() {
+    function renderHeaderMouseover() {
         var leftPercent = this.getAttribute('data-attribute') * 20;
         $('#underline-hover').css('left', leftPercent + '%').show();
-    });
+    }
 
-    $('#header').on('mouseleave', function() {
+    function renderHeaderMouseleave() {
         $('#underline-hover').hide();
-    });
+    }
 
-    $('.header-topic').click(function() {
+    function renderHeaderClick() {
         var leftPercent = this.getAttribute('data-attribute') * 20;
-
         $('#underline').animate({left: leftPercent + '%'}, 200);
-    });
+    }
 
+    function fadeCover() {
+        var coverHeight = $(window).height(),
+            scrollTop = $(window).scrollTop(),
+            imageOpacity = 1 - (scrollTop / coverHeight / 4),
+            nameOpacity = 1 - (scrollTop / coverHeight * 6),
+            socialOpacity = 1 - (scrollTop / coverHeight * 1.7);
 
+        // $('#cover').css({ opacity: imageOpacity });
+        $('#name').css({ opacity: nameOpacity });
+        $('#social').css({ opacity: socialOpacity });
+    }
 
-});
-
+})();
