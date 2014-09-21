b01c42fe0358795d5c5d4bda69de26bcb7375f5a
hmajoros <majoros.henry@gmail.com>
2014-09-21 17:31:33 -0400

    fixed?

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
-from flask.ext.compress import Compress
+# from flask.ext.compress import Compress
+
 
 app = Flask(__name__)
-Compress(app)
+
+# compress = Compress()
+# 
+# def start_app():
+#     app = Flask(__name__)
+#     compress.init_app(app)
+#     return app
+# 
+# app = start_app()
 
 from web import views
