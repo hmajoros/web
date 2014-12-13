b5e5205dee9fccd16655c80b8f81314cf1e404e5
Henry Majoros <majoros.henry@gmail.com>
2014-12-12 20:20:52 -0500

    base responsive layout almost complete

--- web/static/js/beta.js
+++ web/static/js/beta.js
@@ -9,41 +9,58 @@
             winWidth = $(window).width();
 
         $('#sidebar').css({ height: winHeight });
-        $('#sidebar-nav').css({ height: winHeight - 10 - 250 });
-        // $('.content-full').css({ height: winHeight });
+        $('#sidebar-nav').css({ height: winHeight - 10 - 250 }); // TODO: give these numbers names
             
-        $('.sidebar-nav-item').on('click', renderNavClick);
-        $('#nav-btn').on('click', renderHeaderClick);
-        
+        $('.sidebar-nav-item').on('click', renderSidebarClick);
+        $('#nav-btn').on('click', renderHeaderOpen);
         $(window).on('resize', renderPageLayout);
-        
-        
+
         renderPageLayout();        
     });
 
     function renderPageLayout() {
         if ($(window).width() > 768) {
-            $('#sidebar').removeClass('hide');
-            $('#content').addClass('sidebar-open');
+            renderSidebarLayout();
         } else {
-            $('#sidebar').addClass('hide');
-            $('#content').removeClass('sidebar-open');
+            renderHeaderLayout();
         }
     }
-    
-    function renderHeaderClick() {
-        console.warn('headerClick');
-        
-        if (!this.headerIsOpen) {
-            renderHeaderOpen();
+
+    function renderSidebarLayout() {
+        var faceBtn = $('#face');
+
+        if (headerIsOpen) {
+            faceBtn.off().css({ cursor: 'default' });
+            headerIsOpen = false;
+            $('#header-nav').addClass('hide');
+            faceBtn.removeClass('face-hover').css({ left: '30px' });
+            $('#nav-btn').css({ right: '30px' });
+        }
+        $('#content').addClass('sidebar-open');
+        faceBtn.css({ left: '75px' });
+    }
+
+    function renderHeaderLayout() {
+        var faceBtn = $('#face'),
+            faceSize = faceBtn.width(),
+            winWidth = $(window).width(),
+            faceLeft = (winWidth - faceSize) / 2;
+
+        // full width content
+        $('#content').removeClass('sidebar-open');
+
+        // render face in right spot
+        if (headerIsOpen) {
+            faceBtn.css({ left: faceLeft });
         } else {
-            renderHeaderClose();
+            faceBtn.css({ left: '30px' });
         }
     }
     
     function renderHeaderOpen() {
         var faceBtn = $('#face'),
             navBtn = $('#nav-btn'),
+            header = $('#header-nav'),
             winWidth = $(window).width(),
             faceBtnWidth = parseInt(faceBtn.css('width'), 10),
             faceAnimate = (winWidth - faceBtnWidth) / 2,
@@ -54,24 +71,37 @@
         navBtn.animate({right: 0 - faceAnimate}, 200, easing, function() {
             // other animations here
         });
-        
+
+        header.removeClass('hide');
+
         faceBtn.css({cursor: 'pointer'});
+        faceBtn.addClass('face-hover');
         faceBtn.on('click', renderHeaderClose);
+
+        headerIsOpen = true;
     }
     
     function renderHeaderClose() {
         var faceBtn = $('#face'),
             navBtn = $('#nav-btn'),
+            header = $('#header-nav'),
             easing = 'linear';
         
         faceBtn.css({cursor: 'default'});
-        
+        // faceBtn.removeClass('face-hover');
+
+        header.addClass('hide');
+
         faceBtn.animate({left: 30}, 200, easing);
-        navBtn.animate({right: 30}, 200, easing);
+        navBtn.animate({right: 30}, 200, easing, function() {
+            faceBtn.removeClass('face-hover');
+        });
+
+        headerIsOpen = false;
     }
 
 
-    function renderNavClick(ev) {
+    function renderSidebarClick(ev) {
         var xPos = ev.pageX - $(this).offset().left, 
             yPos = ev.pageY - $(this).offset().top,
             r = 30 / 2, // initial circle diameter is 30
