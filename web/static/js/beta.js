e6a18f914100443b6898db94e8218bb4bc01d6bd
Henry Majoros <majoros.henry@gmail.com>
2015-04-22 22:22:09 -0400

    major uphaul to personal site beta

--- web/static/js/beta.js
+++ web/static/js/beta.js
@@ -1,108 +1,43 @@
 (function () {
     
-    var fullLayout = true, // two stages, full or small. represented by boolean
-        headerIsOpen = false,
-        initPosY = 0;
-
     $(document).ready(function() {
         var winHeight = $(window).height(),
             winWidth = $(window).width();
-        
-        renderHeaderImage();
-
-        $('#sidebar').css({ height: winHeight });
-        $('#sidebar-nav').css({ height: winHeight - 10 - 250 }); // TODO: give these numbers names
             
+        $('.header-nav-item').on('click', renderHeaderClose);
+        $('.nav-btn').on('click', renderHeaderOpen);
+        $('.btn-more').on('click', clickCircleTest);
+        $('.btn-less').on('click', closeInfoCnt);
         $('.sidebar-nav-item').on('click', renderSidebarClick);
-        $('.header-nav-item').on('click', renderHeaderClick);
-        $('#nav-btn').on('click', renderHeaderOpen);
         $(window).on('resize', renderPageLayout);
-        $(document).on('scroll', renderImageScroll);
 
-        renderPageLayout();        
+        renderPageLayout();
+        randomizeBackgrounds();
+      
     });
 
-    function renderHeaderImage() {
-        var xPosBg = $(window).width() - 300,
-            yPosBg = 60 - (xPosBg / 5);
-            
-        if (xPosBg <= 515) {
-            yPosBg = 0;
-        }
-
-        $('#img1').css({
-            backgroundSize: xPosBg,
-            backgroundPositionY: yPosBg
-        });
-
-        initYPos = yPosBg;
-    }
-
-    function renderImageScroll() {
-        var scroll = $(document).scrollTop();
-        if (scroll >= 0) {
-            $('#img1').css({ backgroundPositionY: initYPos -0.75 * scroll }); 
-        }
-    }
-
     function renderPageLayout() {
-        if ($(window).width() > 768) {
-            renderHeaderImage();
-            renderSidebarLayout();
-            fullLayout = true;
-        } else {
-            renderHeaderLayout();
-            fullLayout = false;
-        }
-    }
-
-    function renderSidebarLayout() {
-        var faceBtn = $('#face');
-
-        if (headerIsOpen) {
-            faceBtn.off().css({ cursor: 'default' });
-            headerIsOpen = false;
-            $('#header-nav').addClass('hide');
-            faceBtn.removeClass('face-hover').css({ left: '30px' });
-            $('#nav-btn').css({ right: '30px' });
-        }
-        $('#content').addClass('sidebar-open');
-        faceBtn.css({ left: '75px' });
-    }
-
-    function renderHeaderLayout() {
-        var faceBtn = $('#face'),
-            faceSize = faceBtn.width(),
-            winWidth = $(window).width(),
-            faceLeft = (winWidth - faceSize) / 2;
-
-        // full width content
-        $('#content').removeClass('sidebar-open');
-
-        // render face in right spot
-        if (headerIsOpen) {
-            faceBtn.css({ left: faceLeft });
-        } else {
-            faceBtn.css({ left: '30px' });
-        }
+        var height = $(window).height();
+        $('.sidebar').css({ height: height});
+        $('.sidebar-nav').css({ height: height - 160 - 50 - 50 });
     }
     
     function renderHeaderOpen() {
-        var faceBtn = $('#face'),
-            navBtn = $('#nav-btn'),
-            header = $('#header-nav'),
+        var faceBtn = $('.face-header'),
+            navBtn = $('.nav-btn'),
+            header = $('.header-nav'),
             winWidth = $(window).width(),
             faceBtnWidth = parseInt(faceBtn.css('width'), 10),
             faceAnimate = (winWidth - faceBtnWidth) / 2,
             easing = 'linear';
         
         
-        faceBtn.animate({left: faceAnimate}, 200, easing);
-        navBtn.animate({right: 0 - faceAnimate}, 200, easing, function() {
-            // other animations here
+        faceBtn.animate({left: faceAnimate}, 200);
+        navBtn.animate({right: 0 - faceAnimate}, 200, function() {
+            header.css({ marginTop: -500 });
+            header.removeClass('hide');
+            header.animate({ marginTop: 0 }, 200);
         });
-
-        header.removeClass('hide');
 
         faceBtn.css({cursor: 'pointer'});
         faceBtn.addClass('face-hover');
@@ -112,34 +47,91 @@
     }
     
     function renderHeaderClose() {
-        var faceBtn = $('#face'),
-            navBtn = $('#nav-btn'),
-            header = $('#header-nav'),
+        var faceBtn = $('.face-header'),
+            navBtn = $('.nav-btn'),
+            header = $('.header-nav'),
             easing = 'linear';
         
         faceBtn.css({cursor: 'default'});
         // faceBtn.removeClass('face-hover');
 
-        header.addClass('hide');
-
-        faceBtn.animate({left: 30}, 200, easing);
-        navBtn.animate({right: 30}, 200, easing, function() {
-            faceBtn.removeClass('face-hover');
+        header.animate({ marginTop: -500 }, 200, function() {
+            header.addClass('hide');
+            faceBtn.animate({left: 30}, 200, easing);
+            navBtn.animate({right: 0}, 200, easing, function() {
+                faceBtn.removeClass('face-hover');
+            });
         });
+
 
         faceBtn.off();
 
         headerIsOpen = false;
     }
 
-    function renderHeaderClick() {
-        var data = $(this).data('scroll');
-        
-        renderHeaderClose();
-
-        scrollToPage(data);
+    function randomizeBackgrounds() {
+        $('.class-title').each(function() {
+            var rand = Math.floor(Math.random() * 100) + 1;
+            rand = rand + '%';
+            console.warn(rand);
+            $(this).css({ backgroundPositionY: rand });
+        });
     }
 
+    function clickCircleTest() {
+        var self = $(this),
+            parent = self.parent(),
+            more = parent.find('.btn-more'),
+            less = parent.find('.btn-less'),
+            text = parent.find('.main-text'),
+            circle;
+
+        parent.prepend('<div class="click-circle"></div>');
+
+        circle = parent.find('.click-circle');
+
+        console.warn(more);
+        console.warn(less);
+
+        r = 900; // radius of new circle, which fills entire width
+        circle.animate({
+            height: r * 2,
+            width: r * 2,
+            bottom: 0 - r,
+            right: 0 - r
+        }, 300, 'easeInExpo', function() {
+            // TODO: animate rotation fix?
+            text.css({ opacity: 0 }).removeClass('hide');
+            text.animate({ opacity: 1}, 300, 'easeInExpo');
+            more.addClass('hide');
+            less.removeClass('hide');
+        });
+    }
+
+    function closeInfoCnt() {
+        var self = $(this),
+            parent = self.parent(),
+            more = parent.find('.btn-more'),
+            less = parent.find('.btn-less'),
+            text = parent.find('.main-text'),
+            circle = parent.find('.click-circle');
+
+        // TODO: animate text fade out
+        text.animate({ opacity: 0 }, 300, 'easeOutExpo', function() {
+            circle.animate({
+                height: 48,
+                width: 48,
+                bottom: 0,
+                right: 0
+            }, 300, 'easeOutExpo', function() {
+                less.addClass('hide');
+                more.removeClass('hide');
+                circle.remove(); // important, otherwise they just stack
+            });
+
+            text.addClass('hide');
+        });
+    }
 
     function renderSidebarClick(ev) {
         var xPos = ev.pageX - $(this).offset().left, 
@@ -164,20 +156,9 @@
             self.addClass('active');
         });
 
-        scrollToPage(data);
+        // scrollToPage(data);
     }
 
-    function scrollToPage(page) {
-        scrollPos = page ? $('#section' + page).offset().top : 0;
 
-
-        if (!fullLayout && page) {
-            console.warn('hits');
-            scrollPos -= 100;
-        }
-        $('html, body').animate({
-            scrollTop: scrollPos
-        }, 300);
-    }
 
 })();
