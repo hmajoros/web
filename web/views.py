d66557eff31c62ca8beeec9bca4f8ff9dcf37df2
Henry Majoros <majoros.henry@gmail.com>
2015-08-23 20:52:10 -0400

    preparing site to push live.
    - added link to legacy site
    - added new icon button types
    - added link to resume on home page

--- web/views.py
+++ web/views.py
@@ -6,6 +6,10 @@ from web import app
 def index():
     return render_template('index.html', title = 'home')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'about')
+
 @app.route('/legacy')
 def legacy():
     return render_template('legacy.html', title = 'legacy')
