6cf7c22333833862f28ae6583d0d299401a0af55
Henry Majoros <majoros.henry@gmail.com>
2015-09-03 16:38:58 -0400

    adding cards for work page, and an example

--- web/views.py
+++ web/views.py
@@ -10,6 +10,10 @@ def index():
 def about():
     return render_template('about.html', title = 'about')
 
+@app.route('/work')
+def work():
+    return render_template('work.html', title = 'work')
+
 @app.route('/legacy')
 def legacy():
     return render_template('legacy.html', title = 'legacy')
